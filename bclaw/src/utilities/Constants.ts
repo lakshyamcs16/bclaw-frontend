806594e7d861faa6c8d4460c4b50bfb23425cc9d
lakshyasethi <lakshyasethi@carfax.com>
2024-10-06 23:36:08 -0400

    update url

--- bclaw/src/utilities/Constants.ts
+++ bclaw/src/utilities/Constants.ts
@@ -1,3 +1,3 @@
-export const API_BASE_URL = "http://localhost:8000/api";
+export const API_BASE_URL = "http://3.81.73.111:8000/api";
 export const BC_LAW_DOCUMENT_URL = "https://www.bclaws.gov.bc.ca/civix";
 export const FETCH_TIMEOUT_DURATION_MS = 100000;
