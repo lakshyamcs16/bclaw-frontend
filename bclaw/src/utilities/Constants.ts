de85eaf1630614a03bbfd21eace612379c001814
lakshyasethi <lakshyasethi@carfax.com>
2024-10-07 00:06:12 -0400

    update route and gitignore

--- bclaw/src/utilities/Constants.ts
+++ bclaw/src/utilities/Constants.ts
@@ -1,3 +1,3 @@
-export const API_BASE_URL = "http://3.81.73.111:8000/api";
+export const API_BASE_URL = "https://3.81.73.111/api";
 export const BC_LAW_DOCUMENT_URL = "https://www.bclaws.gov.bc.ca/civix";
 export const FETCH_TIMEOUT_DURATION_MS = 100000;
